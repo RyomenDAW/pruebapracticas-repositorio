7602dea1622b5ee30dd8bc87c254fa8e28de5dfd
Javi Cuesta <javicuesta23.jc@gmail.com>
2026-04-29 13:54:44 +0200

    Tarea 3 en comenzada y en proceso

---
+++ La Guia De Sevilla/vue-practica/src/components/TablaContactos.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+contactos: { type: Array, required: true }
+})
+const emit = defineEmits(["contacto-seleccionado"])
+
+
+
+const seleccionar = (contacto) => {
+// Completad vosotros: emitid el evento con el contacto
+emit("contacto-seleccionado", contacto)
+}
+</script>
+<template>
+<table>
+<thead>
+<tr>
+<th>Nombre</th>
+<th>Email</th>
+<th>Teléfono</th>
+<th>Ciudad</th>
+<th>Acción</th>
+</tr>
+</thead>
+<tbody>
+<tr v-for = 
+<!-- v-for sobre props.contactos -->
+<!-- Botón "Ver" que llama a seleccionar(contacto) -->
+
+</tbody>
+</table>
+</template>
